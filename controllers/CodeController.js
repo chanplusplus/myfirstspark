91dda0defd0efed68cbca8fda1ae5275e6e103ac
Chan Pruksapha <chan.pruksapha14@imperial.ac.uk>
2016-05-15 03:17:39 +0700

    Angular v.1.0

---
+++ controllers/CodeController.js
@@ -0,0 +1,18 @@
+    hashtagApp.controller('CodeController', ['$document','$scope',function ($document,$scope) {
+      $scope.value = 0;
+      $scope.section = [];
+      $scope.section.push($document[0].getElementById("section1"));
+      $scope.section.push($document[0].getElementById("section2"));
+      $scope.section.push($document[0].getElementById("section3"));
+      $scope.section.push($document[0].getElementById("section4"));
+      
+      $scope.newValue = function(value) {
+          for(var i=0; i !=$scope.section.length; ++i){
+            var tmp;
+            if(i==value) tmp="block";
+            else tmp="none";
+            $scope.section[i].style.display=tmp;
+          }
+      }
+
+    }]);
